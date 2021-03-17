2f9fbf50f14a9755605093c9e036d8f25a11c7c5
github_username <you@domain.com>
2021-03-17 13:00:01 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='its an wefer project using MLops',
+    author=' kaustubh',
+    license='MIT',
+)
